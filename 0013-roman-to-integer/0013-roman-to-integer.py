474e3a7e98d20941e739753997a0d2d9b98559ba
Anurag Soni <anuragsonni0@gmail.com>
2026-07-05 21:15:46 +0530

    Time: 12 ms (38.32%), Space: 12.3 MB (56.75%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -13,9 +13,6 @@ class Solution(object):
             "D": 500,
             "M": 1000
         }
-        total=0
-        for i in range(len(s)):
-            total += roman[s[i]]
         subtract= {
             "IV": 2,
             "IX": 2,
@@ -24,6 +21,9 @@ class Solution(object):
             "CD": 200,
             "CM": 200
         }
+        total=0
+        for i in range(len(s)):
+            total += roman[s[i]]
         for i in range(len(s)-1):
             che =s[i]+s[i+1]
             if che in subtract:
