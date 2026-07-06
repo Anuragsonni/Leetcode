20409122f7e2f6091bb3d28342cda0f451133b10
Anurag Soni <anuragsonni0@gmail.com>
2026-07-06 07:59:52 +0530

    Time: 11 ms (55.84%), Space: 12.3 MB (53.84%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -6,9 +6,20 @@ class Solution(object):
         """
         if x<0 or (x%10 == 0 and x!=0):
             return False
+        # else :
+        #     rev=0
+        #     while rev < x:
+        #         rev= rev*10+ x%10
+        #         x=x//10
+        #     return rev == x or rev // 10 == x
         else :
-            rev=0
-            while rev < x:
-                rev= rev*10+ x%10
-                x=x//10
-            return rev == x or rev // 10 == x
+            x=str(x)
+            sta=0
+            end=len(x)-1
+            while sta<end:
+                if x[sta] == x[end]:
+                    sta+=1
+                    end-=1
+                else:
+                    return False
+            return True
