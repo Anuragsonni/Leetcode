ac60a785cdfb3d33116bb351c1bfa513ef0b328d
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 01:14:01 +0530

    Time: 11 ms (19.47%), Space: 12.4 MB (80.4%) - LeetHub

--- 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.py
@@ -1,21 +1,29 @@
+from collections import Counter
 class Solution(object):
     def maxNumberOfBalloons(self, text):
         """
         :type text: str
         :rtype: int
         """
-        frequency=[0]* 26
-        for i in text:
-            frequency[ord(i)-ord('a')] +=1
-        dic = {
-            'a': frequency[0],
-            'b': frequency[1],
-            'l': frequency[11]//2,
-            'o': frequency[14]//2,
-            'n': frequency[13]
-        }
-        a=min(dic.values())
-        if a>0:
-            return a
+        # frequency=[0]* 26
+        # for i in text:
+        #     frequency[ord(i)-ord('a')] +=1
+        # dic = {
+        #     'a': frequency[0],
+        #     'b': frequency[1],
+        #     'l': frequency[11]//2,
+        #     'o': frequency[14]//2,
+        #     'n': frequency[13]
+        # }
+        # a=min(dic.values())
+        # if a>0:
+        #     return a
+        # else:
+        #     return 0
+        count= Counter(text)
+        lis=[count['b'],count['a'],count['l']//2,count['o']//2,count['n']]
+        ans=min(lis)
+        if ans>0:
+            return ans
         else:
             return 0
