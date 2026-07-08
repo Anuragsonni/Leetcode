21b04b7bfb44f8fba7252e93139bc1cd003bd17a
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 18:40:51 +0530

    Time: 0 ms (100%), Space: 12.3 MB (59.06%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -5,25 +5,50 @@ class Solution(object):
         :type b: str
         :rtype: str
         """
-        multi=0
-        ans=0
         an=""
-        for i in range(len(a)-1,-1,-1):
-            if a[i]=='1':
-                ans+=2**multi
-            multi+=1
-        multi=0
-        for i in range(len(b)-1,-1,-1):
-            if b[i]=='1':
-                ans+=2**multi
-            multi+=1
-        if ans==0:
-            return "0"
-        while ans>0:
-            r=ans%2
-            if r==1:
-                an+='1'
+        carry=0
+        n= max(len(a),len(b))
+        while n>min(len(a),len(b)) :
+            if len(a)< len(b) :
+                a="0"+ a
             else:
-                an+='0'
-            ans=ans//2
-        return an[::-1]
+                b="0"+ b
+        for i in range(n-1,-1,-1) :
+            total= int(a[i])+int(b[i])+ carry
+            if total==3:
+                an="1"+an
+                carry=1
+            elif total==2:
+                an="0"+an
+                carry=1
+            elif total==1:
+                an="1"+an
+                carry=0
+            else:
+                an="0"+an
+                carry=0  
+        if carry== 1:
+            an="1"+an
+        return an
+        # multi=0
+        # ans=0
+        # an=""
+        # for i in range(len(a)-1,-1,-1):
+        #     if a[i]=='1':
+        #         ans+=2**multi
+        #     multi+=1
+        # multi=0
+        # for i in range(len(b)-1,-1,-1):
+        #     if b[i]=='1':
+        #         ans+=2**multi
+        #     multi+=1
+        # if ans==0:
+        #     return "0"
+        # while ans>0:
+        #     r=ans%2
+        #     if r==1:
+        #         an+='1'
+        #     else:
+        #         an+='0'
+        #     ans=ans//2
+        # return an[::-1]
