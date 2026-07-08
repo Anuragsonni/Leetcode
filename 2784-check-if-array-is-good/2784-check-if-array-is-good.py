1eccda7793dd0ca7489f58749e15b52dc46be435
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 15:59:27 +0530

    Time: 0 ms (100%), Space: 12.4 MB (18.08%) - LeetHub

--- 2784-check-if-array-is-good/2784-check-if-array-is-good.py
+++ 2784-check-if-array-is-good/2784-check-if-array-is-good.py
@@ -5,5 +5,17 @@ class Solution(object):
         :type nums: List[int]
         :rtype: bool
         """
-        nums.sort()
-        return nums== list(range(1,len(nums)))+ [len(nums)-1]
+        # nums.sort()
+        # return nums== list(range(1,len(nums)))+ [len(nums)-1]
+        l = len(nums)
+
+        if max(nums) != l - 1:
+            return False
+
+        for i in range(1, l):
+            if i not in nums:
+                return False
+
+        nums.remove(l - 1)
+
+        return (l - 1) in nums
