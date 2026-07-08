a86f2f4186e3bb72b55999c54911684be3c06e11
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 18:43:31 +0530

    Time: 0 ms (100%), Space: 12.7 MB (18.89%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,21 @@
+class Solution(object):
+    def generateParenthesis(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        res = []
+
+        def backtrack(curr, open, close):
+            if len(curr) == 2 * n:
+                res.append(curr)
+                return
+
+            if open < n:
+                backtrack(curr + "(", open + 1, close)
+
+            if close < open:
+                backtrack(curr + ")", open, close + 1)
+
+        backtrack("", 0, 0)
+        return res
