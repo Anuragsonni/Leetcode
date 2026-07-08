12b332a3a5f5109f5ce9d444176606425311f502
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 16:15:17 +0530

    Time: 43 ms (5.4%), Space: 12.8 MB (3.9%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def separateDigits(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[int]
+        """
+        ans=[]
+        for num in nums:
+            temp=[]
+            while num>0:
+                temp.append(num%10)
+                num=num//10
+            temp.reverse()
+            ans=ans+temp
+        return ans
