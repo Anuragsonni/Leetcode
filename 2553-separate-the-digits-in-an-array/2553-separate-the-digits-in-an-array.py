4ec7902374ff60746d0000bb336ba66010c2c27f
Anurag Soni <anuragsonni0@gmail.com>
2026-07-08 16:25:22 +0530

    Time: 6 ms (91.16%), Space: 12.8 MB (31.95%) - LeetHub

--- 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.py
@@ -5,14 +5,14 @@ class Solution(object):
         :rtype: List[int]
         """
         ans=[]
-        # for num in nums:
-        #     temp=[]
-        #     while num>0:
-        #         temp.append(num%10)
-        #         num=num//10
-        #     temp.reverse()
-        #     ans.extend(temp)
-        # return ans
         for num in nums:
-            ans.extend(int(s) for s in str(num))
+            temp=[]
+            while num>0:
+                temp.append(num%10)
+                num=num//10
+            temp.reverse()
+            ans.extend(temp)
         return ans
+        # for num in nums:
+        #     ans.extend(int(s) for s in str(num))
+        # return ans
