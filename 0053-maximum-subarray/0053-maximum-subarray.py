982b19677a619b422c6b475f9742938ec24a3cb0
Anurag Soni <anuragsonni0@gmail.com>
2026-07-09 12:37:14 +0530

    Time: 21 ms (98.52%), Space: 22.1 MB (11.61%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -4,10 +4,21 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        max_sum=current_sum=nums[0]
-        for num in nums[1:]:
-            current_sum = max(num, current_sum+num)
-            max_sum = max(max_sum, current_sum)
+        max_sum=nums[0]
+        current_sum=0
+        for num in nums:
+            current_sum += num
+            if current_sum> max_sum:
+                max_sum = current_sum
+            if current_sum<0:
+                current_sum = 0
+        
         return max_sum
+
+        # if not nums:
+        #     return 0
+        # max_sum=current_sum=nums[0]
+        # for num in nums[1:]:
+        #     current_sum = max(num, current_sum+num)
+        #     max_sum = max(max_sum, current_sum)
+        # return max_sum
