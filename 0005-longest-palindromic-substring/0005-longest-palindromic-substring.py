fbe8988221b68a8b1edef54e29e279165b917819
Anurag Soni <anuragsonni0@gmail.com>
2026-07-10 20:33:35 +0530

    Time: 6 ms (99.99%), Space: 19.2 MB (69.15%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,26 +1,53 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        res=""
-        reslen=0
-        for i in range(len(s)):
-            # for even palandrome
-            r, l = i + 1, i 
+        n = len(s)
 
-            while l>-1 and r<len(s) and (s[l] == s[r]):
-                if r-l+1>reslen:
-                    res= s[l:r+1]
-                    reslen= r-l+1
-                # update
-                r, l = r+1, l-1
+        if n <= 1 or s == s[::-1]:
+            return s
+
+        start = 0
+        longest_length = 1
+
+        for end in range(1, n):
+
+            even_start = end - longest_length
+            odd_start = even_start - 1
+
+            odd_candidate = s[odd_start : end + 1]
+            even_candidate = s[even_start : end + 1]
+
+            if odd_start >= 0 and odd_candidate == odd_candidate[::-1]:
+                start = odd_start
+                longest_length += 2
+
+            elif even_candidate == even_candidate[::-1]:
+                start = even_start
+                longest_length += 1
+
+        return s[start : start + longest_length]
+
+
+        # res=""
+        # reslen=0
+        # for i in range(len(s)):
+        #     # for even palandrome
+        #     r, l = i + 1, i 
+
+        #     while l>-1 and r<len(s) and (s[l] == s[r]):
+        #         if r-l+1>reslen:
+        #             res= s[l:r+1]
+        #             reslen= r-l+1
+        #         # update
+        #         r, l = r+1, l-1
             
-            # for odd palandrome
-            r, l = i, i
+        #     # for odd palandrome
+        #     r, l = i, i
 
-            while l>-1 and r<len(s) and (s[l] == s[r]):
-                if r-l+1>reslen:
-                    res= s[l:r+1]
-                    reslen= r-l+1
-                # update
-                r, l = r+1, l-1
+        #     while l>-1 and r<len(s) and (s[l] == s[r]):
+        #         if r-l+1>reslen:
+        #             res= s[l:r+1]
+        #             reslen= r-l+1
+        #         # update
+        #         r, l = r+1, l-1
         
-        return res
+        # return res
