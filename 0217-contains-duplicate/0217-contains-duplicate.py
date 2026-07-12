eca0915a63bf960ac660ac3f7bb2b58ff8efb602
Anurag Soni <anuragsonni0@gmail.com>
2026-07-12 23:29:03 +0530

    Time: 7 ms (89.05%), Space: 32 MB (62%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -7,9 +7,10 @@ class Solution:
         #         return True
         # return False
 
-        nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i] == nums[i+1]:
-                return True
+        # nums.sort()
+        # for i in range(len(nums)-1):
+        #     if nums[i] == nums[i+1]:
+        #         return True
         
-        return False
+        # return False
+        return len(nums)!=len(set(nums))
