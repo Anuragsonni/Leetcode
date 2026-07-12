9c69cd62f55270ec04060cb8834c0035628f5587
Anurag Soni <anuragsonni0@gmail.com>
2026-07-12 13:27:36 +0530

    Time: 14 ms (29.87%), Space: 21 MB (72.58%) - LeetHub

--- 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
+++ 0977-squares-of-a-sorted-array/0977-squares-of-a-sorted-array.py
@@ -4,19 +4,36 @@ class Solution:
         # return sorted(num)
 
         # three pointer approch 
-        l, r, w = 0, len(nums)-1, len(nums)-1
-        lis=[0]*len(nums)
-        while l <= r :
-            if nums[l]**2 < nums[r]**2:
-                lis[w]= nums[r]**2
-                r-=1
-            else :
-                lis[w]= nums[l]**2
-                l+=1
-            w-=1
+        # l, r, w = 0, len(nums)-1, len(nums)-1
+        # lis=[0]*len(nums)
+        # while l <= r :
+        #     if nums[l]**2 < nums[r]**2:
+        #         lis[w]= nums[r]**2
+        #         r-=1
+        #     else :
+        #         lis[w]= nums[l]**2
+        #         l+=1
+        #     w-=1
             # else :
             #     lis[w], lis[w-1]= nums[r]**2, nums[r]**2
             #     w-=2
             #     l+=1 
             #     r-=1
-        return lis
+        # return lis
+
+
+        
+        l, r = 0, len(nums) - 1
+        res = []
+        
+        while l <= r:
+            # Using absolute value is slightly faster than calculating exponents twice
+            if abs(nums[l]) > abs(nums[r]):
+                res.append(nums[l] * nums[l])
+                l += 1
+            else:
+                res.append(nums[r] * nums[r])
+                r -= 1
+                
+        # Reversing a list in Python is heavily optimized in C
+        return res[::-1]
