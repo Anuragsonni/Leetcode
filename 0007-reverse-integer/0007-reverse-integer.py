5f351bf946e79da83171f6209cc5855ac8f492b5
Anurag Soni <anuragsonni0@gmail.com>
2026-07-12 11:55:53 +0530

    Time: 52 ms (19.45%), Space: 19.2 MB (69.15%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -0,0 +1,18 @@
+class Solution:
+    def reverse(self, x: int) -> int:
+        
+        negetive= x<=0
+        x= abs(x) 
+        ans=0
+
+        while x>0:
+            ans= ans*10 +x%10 
+            x=x//10
+        
+        if negetive:
+            ans = -ans
+
+        if ans > 2147483647 or ans < -2147483648:
+            return 0
+
+        return ans
