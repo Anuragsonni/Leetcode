f11c5d49dd58c24e723c0d6ba1d89e633c711963
Anurag Soni <anuragsonni0@gmail.com>
2026-07-12 12:23:28 +0530

    Time: 48 ms (43.65%), Space: 19.2 MB (69.15%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -1,32 +1,32 @@
 class Solution:
     def reverse(self, x: int) -> int:
-        negetive= x<=0
-        x= str(abs(x))
-        x= int(x[::-1])
-
-        if negetive:
-            x= -x
-        
-        if x > 2147483647 or x < -2147483648:
-            return 0
-
-        return x
-
-
-
-        
         # negetive= x<=0
-        # x= abs(x) 
-        # ans=0
+        # x= str(abs(x))
+        # x= int(x[::-1])
 
-        # while x>0:
-        #     ans= ans*10 +x%10 
-        #     x=x//10
-        
         # if negetive:
-        #     ans = -ans
-
-        # if ans > 2147483647 or ans < -2147483648:
+        #     x= -x
+        
+        # if x > 2147483647 or x < -2147483648:
         #     return 0
 
-        # return ans
+        # return x
+
+
+
+        
+        negetive= x<=0
+        x= abs(x) 
+        ans=0
+
+        while x>0:
+            ans= ans*10 +x%10 
+            x=x//10
+
+            if ans > 2147483647 :
+                return 0
+        
+        if negetive:
+            ans = -ans
+
+        return ans
