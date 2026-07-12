1e7a79786a371c47cab420fd1539a4d53470f5dc
Anurag Soni <anuragsonni0@gmail.com>
2026-07-12 14:25:38 +0530

    Time: 31 ms (7.4%), Space: 20.7 MB (15.31%) - LeetHub

--- 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
+++ 0345-reverse-vowels-of-a-string/0345-reverse-vowels-of-a-string.py
@@ -1,21 +1,40 @@
 class Solution:
     def reverseVowels(self, s: str) -> str:
         # ch="".join(filter(self.is_vow, s))
-        seen= []
-        index=set()
-        for i, ch in enumerate(s):
-            if self.is_vow(ch):
-                seen.append(ch)
-                index.add(i)
+        # seen= []
+        # index=set()
+        # for i, ch in enumerate(s):
+        #     if self.is_vow(ch):
+        #         seen.append(ch)
+        #         index.add(i)
+        # ch=["#"]*len(s)
+        # for i in range(len(s)):
+        #     if i in index:
+        #         ch[i]= seen[-1]
+        #         seen.pop()
+        #     else:
+        #         ch[i]=s[i]
+
+        # return "".join(ch)
+
+        l, r = 0, len(s)-1
         ch=["#"]*len(s)
-        for i in range(len(s)):
-            if i in index:
-                ch[i]= seen[-1]
-                seen.pop()
+        while l<=r:
+            if self.is_vow(s[l]):
+
+                if self.is_vow(s[r]):
+                    ch[l], ch[r]=s[r], s[l]
+                    l+=1
+                    r-=1
+                else:
+                    ch[r]=s[r]
+                    r-=1
             else:
-                ch[i]=s[i]
+                ch[l]= s[l]
+                l+=1
 
         return "".join(ch)
+
 
 
     def is_vow(self,ch):
