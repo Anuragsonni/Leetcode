4b663ac468f90091769fda261dcda8538f7c81cf
Anurag Soni <anuragsonni0@gmail.com>
2026-07-14 22:51:13 +0530

    Time: 28 ms (18.2%), Space: 19.7 MB (8.14%) - LeetHub

---
+++ 0383-ransom-note/0383-ransom-note.py
@@ -0,0 +1,15 @@
+class Solution:
+    def canConstruct(self, ransomNote: str, magazine: str) -> bool:
+        frequency=[0]*26
+        for i in magazine :
+            frequency[ord(i)- ord('a')-1] += 1
+
+        for i in ransomNote :
+            frequency[ord(i)- ord('a')-1] -= 1
+        
+        for i in frequency :
+
+            if i<0 :
+                return False
+        
+        return True
