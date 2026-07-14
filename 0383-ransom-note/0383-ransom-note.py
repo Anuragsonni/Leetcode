33fa86fe468b0829a9f1df9286969d2f90800e07
Anurag Soni <anuragsonni0@gmail.com>
2026-07-14 22:55:50 +0530

    Time: 0 ms (100%), Space: 19.6 MB (33%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,15 +1,22 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        frequency=[0]*26
-        for i in magazine :
-            frequency[ord(i)- ord('a')-1] += 1
+        # frequency=[0]*26
+        # for i in magazine :
+        #     frequency[ord(i)- ord('a')-1] += 1
 
-        for i in ransomNote :
-            frequency[ord(i)- ord('a')-1] -= 1
+        # for i in ransomNote :
+        #     frequency[ord(i)- ord('a')-1] -= 1
         
-        for i in frequency :
+        # for i in frequency :
 
-            if i<0 :
+        #     if i<0 :
+        #         return False
+        
+        # return True
+
+        for i in set(ransomNote) :
+
+            if magazine.count(i) < ransomNote.count(i) :
                 return False
         
         return True
