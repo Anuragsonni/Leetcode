4f3d96e68d107337e0813c69b9e75369b1630ec9
Anurag Soni <anuragsonni0@gmail.com>
2026-07-17 20:37:46 +0530

    Time: 36 ms (95.16%), Space: 19.1 MB (88.02%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.py
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.py
@@ -1,22 +1,36 @@
 class Solution:
     def mergeAlternately(self, word1: str, word2: str) -> str:
-        if not word1:
-            return word2
-        if not word2:
-            return word1
-        result=""
-        i, j = 0, 0
-        for index in range(len(word1)+len(word2)):
-            if i==len(word1):
-                return result + word2[j:]
-            if j==len(word2):
-                return result + word1[i:]    
+        # if not word1:
+        #     return word2
+        # if not word2:
+        #     return word1
+        # result=""
+        # i, j = 0, 0
+        # for index in range(len(word1)+len(word2)):
+        #     if i==len(word1):
+        #         return result + word2[j:]
+        #     if j==len(word2):
+        #         return result + word1[i:]    
             
-            if index%2 :
-                result += word2[j]
-                j+=1
-            else:
-                result += word1[i]
-                i+=1
+        #     if index%2 :
+        #         result += word2[j]
+        #         j+=1
+        #     else:
+        #         result += word1[i]
+        #         i+=1
         
-        return result
+        # return result
+        
+        result = []
+        i = j = 0
+
+        while i < len(word1) and j < len(word2):
+            result.append(word1[i])
+            result.append(word2[j])
+            i += 1
+            j += 1
+
+        result.extend(word1[i:])
+        result.extend(word2[j:])
+
+        return "".join(result)
