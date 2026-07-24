98c4f7b9d227ad1c95e0338112f1a6cd478de59a
Anurag Soni <anuragsonni0@gmail.com>
2026-07-24 19:08:10 +0530

    Time: 0 ms (100%), Space: 12.6 MB (33.93%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.py
@@ -1,24 +1,24 @@
-class Solution:
-    def findMin(self, nums: List[int]) -> int:
-        l, r = 0, len(nums) - 1
-        smallest = float('inf')  # Use infinity instead of hardcoded 5000
-
-        while l <= r:
-            # OPTIMIZATION: If the current range is already sorted,
-            # the leftmost element is guaranteed to be the minimum!
-            if nums[l] <= nums[r]:
-                smallest = min(smallest, nums[l])
-                break
-
-            mid = (l + r) // 2
-            smallest = min(smallest, nums[mid])
-
-            # Left half is sorted -> smallest in left half is nums[l], search right half
-            if nums[mid] >= nums[l]:
-                smallest = min(smallest, nums[l])
+class Solution(object):
+    def findMin(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        l, r = 0, len(nums)-1
+        smallest = 5000 
+        while l<=r :
+            mid = (l+r)//2 
+            if nums[l] < nums[r] :
+                return nums[l]
+            
+            # left part is sorted 
+            if nums[l] <= nums[mid]:
+                smallest = min(nums[l],smallest)
                 l = mid + 1
-            # Right half is unsorted/pivot is on the left -> search left half
-            else:
-                r = mid - 1
-
+            
+            # right part is sorted 
+            else :
+                smallest = min(nums[mid], smallest)
+                r = mid
+        
         return smallest
