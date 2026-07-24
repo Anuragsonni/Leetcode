1c9d3628006b8ad1e2fa865a452f88b6f2195373
Anurag Soni <anuragsonni0@gmail.com>
2026-07-24 12:02:14 +0530

    Time: 2 ms (59.81%), Space: 12.4 MB (54.17%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,18 +1,17 @@
-class Solution:
-    def mySqrt(self, x: int) -> int:
-        l, r = 0, x 
+class Solution(object):
+    def mySqrt(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        l, r = 0, x
         while l<=r :
-            mid = (l+r)//2
-            sq = mid * mid
-            if sq == x :
-                return mid
-            elif sq > x:
-                r=mid-1
+            mid = (l+r) // 2 
+            sq = mid * mid 
+            if sq > x:
+                r = mid - 1 
+            elif sq < x:
+                l = mid +1 
             else:
-                l=mid+1
+                return mid
         return r
-        # if x==0 or x==1:
-        #     return x
-        # for i in range(1,x+1):
-        #     if i*i > x:
-        #         return i-1
