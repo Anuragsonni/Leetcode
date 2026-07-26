c4fef3bc217980d27801e8078a162c8e24693d65
Anurag Soni <anuragsonni0@gmail.com>
2026-07-26 13:41:36 +0530

    Time: 0 ms (100%), Space: 20.2 MB (98.77%) - LeetHub

--- 0206-reverse-linked-list/0206-reverse-linked-list.py
+++ 0206-reverse-linked-list/0206-reverse-linked-list.py
@@ -5,26 +5,35 @@
 #         self.next = next
 class Solution:
     def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        if not head: 
-            return head
-        temp_head = ListNode()
-        r_head= temp_head
+        pre = None
+        curr = head
+        while head:
+            curr = head
+            head = head.next
+            curr.next = pre 
+            pre = curr
+        
+        return curr
+        # if not head: 
+        #     return head
+        # temp_head = ListNode()
+        # r_head= temp_head
         
 
-        while temp_head:
-            tail = head
-            afterTail = head.next
-            if afterTail != None :
-                while afterTail.next:
-                    afterTail = afterTail.next
-                    tail = tail.next
+        # while temp_head:
+        #     tail = head
+        #     afterTail = head.next
+        #     if afterTail != None :
+        #         while afterTail.next:
+        #             afterTail = afterTail.next
+        #             tail = tail.next
 
-                temp_head.next = afterTail
-                temp_head = temp_head.next
-                tail.next = None
-            else:
-                temp_head.next = tail
-                temp_head = temp_head.next
-                temp_head = None
+        #         temp_head.next = afterTail
+        #         temp_head = temp_head.next
+        #         tail.next = None
+        #     else:
+        #         temp_head.next = tail
+        #         temp_head = temp_head.next
+        #         temp_head = None
 
-        return r_head.next          
+        # return r_head.next          
