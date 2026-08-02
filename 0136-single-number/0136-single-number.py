0f74bd01c7cbf686b25eefd0fea2bedca70bfed1
Anurag Soni <anuragsonni0@gmail.com>
2026-08-02 22:21:55 +0530

    Time: 4842 ms (5.02%), Space: 13.9 MB (52.1%) - LeetHub

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,11 +1,15 @@
-from collections import Counter
+# from collections import Counter
 class Solution(object):
     def singleNumber(self, nums):
         """
         :type nums: List[int]
         :rtype: int
         """
-        count= Counter(nums)
-        for i in count:
-            if count[i] == 1 :
-                return i 
+        for i in nums :
+            if nums.count(i)==1:
+                return i
+        
+        # count= Counter(nums)
+        # for i in count:
+        #     if count[i] == 1 :
+        #         return i 
